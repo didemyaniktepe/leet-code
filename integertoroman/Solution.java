810e5fca3e139b81bee5d580b9e5952671b69aaa
didem.yaniktepe <didem.yaniktepe@tubitak.gov.tr>
2022-02-08 13:58:31 +0300

    solution of roman to integer

--- integertoroman/Solution.java
+++ integertoroman/Solution.java
@@ -24,9 +24,4 @@ public class Solution {
     }
 
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().intToRoman(25));
-    }
-
-
 }
---
+++ romantointeger/Solution.java
@@ -0,0 +1,31 @@
+package romantointeger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    Map<Character, Integer> romans = new HashMap<>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int number = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (romans.get(s.charAt(i)) >= romans.get(s.charAt(i + 1))) {
+                number += romans.get(s.charAt(i));
+            } else {
+                number -= romans.get(s.charAt(i));
+            }
+        }
+        number += romans.get(s.charAt(s.length()-1));
+        return number;
+    }
+
+}
