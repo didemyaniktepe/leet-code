8b9b77eaba58a485c05f82f512c7cdd9c89bc9ef
didem.yaniktepe <didem.yaniktepe@tubitak.gov.tr>
2022-02-08 09:33:32 +0300

    solution of integer to roman

---
+++ integertoroman/Solution.java
@@ -0,0 +1,32 @@
+package integertoroman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+    List<String> strings = List.of("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
+    List<Integer> integers = List.of(1000,900,500,400,100,90,50,40,10,9,5,4,1);
+
+    public String intToRoman(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println(strings.size());
+        System.out.println(integers.size());
+        for(int i = 0 ; i < integers.size() ; i++ ) {
+            while (num >= integers.get(i)) {
+                num -= integers.get(i);
+                stringBuilder.append(strings.get(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().intToRoman(25));
+    }
+
+
+}
